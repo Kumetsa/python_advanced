b0e4118abfe853977b3b3267ca2736f8b0822bc3
Kumetsa <i.e.ivanov1989@gmail.com>
2023-05-30 22:50:18 +0300

    2 exer

---
+++ 11_Exercise Error Handling/02_email_validator.py
@@ -0,0 +1,49 @@
+import re
+
+
+class NameTooShortError(Exception):
+    pass
+
+
+class MustContainAtSymbolError(Exception):
+    pass
+
+
+class InvalidDomainError(Exception):
+    pass
+
+
+class TooManyAtSymbolsError(Exception):
+    pass
+
+
+class NameCannotBeOnlyNonAlphaNumbericalError(Exception):
+    pass
+
+
+VALID_DOMAINS = r"\.(com|bg|org|net)$"
+
+while True:
+    email = input("Enter your email:")
+    name = email.split("@")[0]
+    match = re.search(VALID_DOMAINS, email)
+
+    if email == "End":
+        break
+
+    if email.count("@") > 1:
+        raise TooManyAtSymbolsError("Maximum one @ symbol allowed.")
+
+    if "@" not in email:
+        raise MustContainAtSymbolError("Email must contain @")
+
+    if len(name) < 4:
+        raise NameTooShortError("Name must be more than 4 characters")
+
+    if not name.isalnum():
+        raise NameCannotBeOnlyNonAlphaNumbericalError("Name cannot consists only of non-alphanumeric characters.")
+
+    if not match:
+        raise InvalidDomainError("Domain must be one of the following: .com, .bg, .org,.net")
+
+    print("Email is valid")
