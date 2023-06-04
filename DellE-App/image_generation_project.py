ec619de449a1028ad3b016cfb7c07e94f65d9d0b
Kumetsa <i.e.ivanov1989@gmail.com>
2023-06-04 23:33:08 +0300

    added Save function
    shown by DiyanK

--- DellE-App/image_generation_project.py
+++ DellE-App/image_generation_project.py
@@ -1,10 +1,14 @@
+import os
+import re
 import urllib.request
 from io import BytesIO
 from PIL import Image, ImageTk
 import openai
 import tkinter as tk
+import random
 
-openai.api_key = "PUT KEY HERE"
+openai.api_key = "PUT_KEY_HERE"
+MEDIA_FOLDER = "images/"
 
 
 def get_image_url():
@@ -18,26 +22,61 @@ def get_image_url():
     return image_url
 
 
-def display_image(image_url):
+def covert_to_image_object(image_url):
     with urllib.request.urlopen(image_url) as url:
         image_data = url.read()
 
-    image_stream = BytesIO(image_data)  # Това е информацията за снимката(не кактоможем да я видим) просто е в паметта
+    image_stream = BytesIO(image_data)  # Това е информацията за снимката(не както можем да я видим) просто е в паметта
+    image = Image.open(image_stream)
 
-    image = ImageTk.PhotoImage(Image.open(image_stream))  # създаваме снимката като реален обект
-    image_label.configure(image=image)
-    image_label.image = image  # за да не изтрие снимката, тъй като има garbage collector
+    return image
+
+
+def display_image(image):
+    tk_image = ImageTk.PhotoImage(image)  # създаваме снимката като реален обект
+
+    image_label.configure(image=tk_image)
+    image_label.image = tk_image  # за да не изтрие снимката, тъй като има garbage collector
+
+
+def save_image(image, path):
+    while os.path.isfile(path):
+        image_name = re.match(r"images/(.* .?)\.jpg", path[1])
+        new_name = image_name + str(random.randint(1, 1_000_000))
+        path = path.replace(image_name, new_name)
+
+    image.save(path)
 
 
 def render_image():
+    global save_button
+
     try:
+        save_button.destroy()
+
         image_url = get_image_url()
+        image_name = "_".join(input_field.get().split()) + ".jpg"
+
         input_field.delete(0, tk.END)
+
+        image = covert_to_image_object(image_url)
+
+        display_image(image)
+
+        save_button = tk.Button(
+            window,
+            text="Save",
+            height=1,
+            command=lambda: save_image(image, os.path.join(MEDIA_FOLDER, image_name))
+        )
+
+        save_button.place(x=350, y=17)
+
     except openai.error.InvalidRequestError:
         error_label = tk.Label(window, text="Prompt cannot be empty!", fg="red")
-        error_label.place(x=175,y=50)
-    else:
-        display_image(image_url)
+        error_label.place(x=175, y=50)
+    # else:
+    #     display_image(image_url)
 
 
 window = tk.Tk()
@@ -54,5 +93,7 @@ generate_button = tk.Button(window, text="Create", height=1,
                             command=render_image)  # funcion is placed without (), or we use lambda x: render_image(
 # parameters)
 generate_button.place(x=300, y=17)
+
+save_button = tk.Button(window, text="Save", height=1)
 
 window.mainloop()
