0e047b121926022de92c719b1811043d39f64891
Kumetsa <i.e.ivanov1989@gmail.com>
2023-06-15 20:31:23 +0300

    ruber duck task

---
+++ 16_Exam_preparation/08_rubber_duck_debuggers.py
@@ -0,0 +1,40 @@
+from collections import deque
+
+programmer_time = deque(map(int, input().split()))
+number_of_tasks = deque(map(int, input().split()))
+
+ducks = {
+    "Darth Vader Ducky": 0,
+    "Thor Ducky": 0,
+    "Big Blue Rubber Ducky": 0,
+    "Small Yellow Rubber Ducky": 0,
+}
+
+total_ducks = 0
+
+while programmer_time and number_of_tasks:
+    current_time = programmer_time.popleft()
+    current_task = number_of_tasks.pop()
+
+    result = current_time * current_task
+
+    if result > 240: #TODO what if it is equal?
+        programmer_time.append(current_time)
+        number_of_tasks.append(current_task - 2)
+    elif 0 < result <= 60:
+        ducks["Darth Vader Ducky"] += 1
+        total_ducks += 1
+    elif 61 <= result <= 120:
+        ducks["Thor Ducky"] += 1
+        total_ducks += 1
+    elif 121 <= result <= 180:
+        ducks["Big Blue Rubber Ducky"] += 1
+        total_ducks += 1
+    elif 181 <= result <= 240:
+        ducks["Small Yellow Rubber Ducky"] += 1
+        total_ducks += 1
+
+print(f"Congratulations, all tasks have been completed! Rubber ducks rewarded:")
+
+for duck, quantity in ducks.items():
+    print(f"{duck}: {quantity}")
