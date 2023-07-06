aa61885a2c01127f8d581980ebf7a928ae2f9d65
Kumetsa <i.e.ivanov1989@gmail.com>
2023-07-06 22:36:22 +0300

    library exercise

---
+++ 02_OOP/04_classes_and_objects_exercise/08_library/project/user.py
@@ -0,0 +1,17 @@
+from typing import List
+
+
+class User:
+    books = []
+
+    def __init__(self, user_id: int, username: str):
+        self.user_id = user_id
+        self.username = username
+        self.books: List[str] = []
+
+    def info(self):
+        return ", ".join(sorted(self.books))
+
+    def __str__(self):
+        return f"{self.user_id}, {self.username}, {self.books}"
+
