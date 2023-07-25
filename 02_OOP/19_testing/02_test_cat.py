0caea6759f1b74fd03be6285590809e28664ffaf
Kumetsa <i.e.ivanov1989@gmail.com>
2023-07-25 14:41:47 +0300

    exercise 2

--- 02_OOP/19_testing/02_test_cat.py
+++ 02_OOP/19_testing/02_test_cat.py
@@ -1,3 +1,7 @@
+import unittest
+from unittest import TestCase
+
+
 class Cat:
     def __init__(self, name):
         self.name = name
@@ -18,7 +22,71 @@ class Cat:
         self.sleepy = False
 
 
-from unittest import TestCase, main
+class CatTests(TestCase):
+    def setUp(self) -> None:
+        self.cat = Cat("test")
+
+    def test_initialize_cat(self):
+        self.assertEqual("test", self.cat.name)
+        self.assertFalse(self.cat.fed)
+        self.assertFalse(self.cat.sleepy)
+        self.assertEqual(0, self.cat.size)
+
+    def test_cat_eats(self):
+        self.assertFalse(self.cat.fed)
+        self.assertFalse(self.cat.sleepy)
+        self.assertEqual(0, self.cat.size)
+
+        self.cat.eat()
+
+        self.assertTrue(self.cat.fed)
+        self.assertTrue(self.cat.sleepy)
+        self.assertEqual(1, self.cat.size)
+
+    def test_cat_has_eaten_raises(self):
+        self.assertFalse(self.cat.fed)
+        self.assertFalse(self.cat.sleepy)
+        self.assertEqual(0, self.cat.size)
+
+        # Arrange
+        self.cat.eat()
+
+        self.assertTrue(self.cat.fed)
+        self.assertTrue(self.cat.sleepy)
+        self.assertEqual(1, self.cat.size)
+
+        # Act
+        with self.assertRaises(Exception) as ex:
+            self.cat.eat()
+
+        self.assertEqual('Already fed.', str(ex.exception))
+
+        self.assertTrue(self.cat.fed)
+        self.assertTrue(self.cat.sleepy)
+        self.assertEqual(1, self.cat.size)
+
+    def test_cat_tries_to_sleep_not_fed_raises(self):
+        self.assertFalse(self.cat.fed)
+        self.assertFalse(self.cat.sleepy)
+
+        with self.assertRaises(Exception) as ex:
+            self.cat.sleep()
+
+        self.assertEqual('Cannot sleep while hungry', str(ex.exception))
+
+        self.assertFalse(self.cat.fed)
+        self.assertFalse(self.cat.sleepy)
+
+    def test_cat_is_not_hungry_can_go_to_sleep(self):
+        self.cat.eat()
+
+        self.assertTrue(self.cat.fed)
+        self.assertTrue(self.cat.sleepy)
+
+        self.cat.sleep()
+
+        self.assertFalse(self.cat.sleepy)
 
 
-
+if __name__ == "__main__":
+    unittest.main()
