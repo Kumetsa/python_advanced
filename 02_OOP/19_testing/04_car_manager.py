bdb734f1558424151bea03ffec43d24d79923b4d
Kumetsa <i.e.ivanov1989@gmail.com>
2023-07-27 00:38:12 +0300

    lab 4

--- 02_OOP/19_testing/04_car_manager.py
+++ 02_OOP/19_testing/04_car_manager.py
@@ -1,76 +1,112 @@
-class Car:
-    def __init__(self, make, model, fuel_consumption, fuel_capacity):
-        self.make = make
-        self.model = model
-        self.fuel_consumption = fuel_consumption
-        self.fuel_capacity = fuel_capacity
-        self.fuel_amount = 0
-    
-    @property
-    def make(self):
-        return self.__make
-    
-    @make.setter
-    def make(self, new_value):
-        if not new_value:
-            raise Exception("Make cannot be null or empty!")
-        self.__make = new_value
+from unittest import TestCase
+import unittest
 
-    @property
-    def model(self):
-        return self.__model
-    
-    @model.setter
-    def model(self, new_value):
-        if not new_value:
-            raise Exception("Model cannot be null or empty!")
-        self.__model = new_value
+# from CarManager.car_manager import Car
 
-    @property
-    def fuel_consumption(self):
-        return self.__fuel_consumption
-    
-    @fuel_consumption.setter
-    def fuel_consumption(self, new_value):
-        if new_value <= 0:
-            raise Exception("Fuel consumption cannot be zero or negative!")
-        self.__fuel_consumption = new_value
 
-    @property
-    def fuel_capacity(self):
-        return self.__fuel_capacity
-    
-    @fuel_capacity.setter
-    def fuel_capacity(self, new_value):
-        if new_value <= 0:
-            raise Exception("Fuel capacity cannot be zero or negative!")
-        self.__fuel_capacity = new_value
+class TestCar(TestCase):
+    def setUp(self) -> None:
+        self.lambo = Car("Lamborghini", "Gallardo", 10, 80)
 
-    @property
-    def fuel_amount(self):
-        return self.__fuel_amount
-    
-    @fuel_amount.setter
-    def fuel_amount(self, new_value):
-        if new_value < 0:
-            raise Exception("Fuel amount cannot be negative!")
-        self.__fuel_amount = new_value
+    def test_correct_car_initialization(self):
+        self.assertEqual("Lamborghini", self.lambo.make)
+        self.assertEqual("Gallardo", self.lambo.model)
+        self.assertEqual(10, self.lambo.fuel_consumption)
+        self.assertEqual(80, self.lambo.fuel_capacity)
+        self.assertEqual(0, self.lambo.fuel_amount)
 
-    def refuel(self, fuel):
-        if fuel <= 0:
-            raise Exception("Fuel amount cannot be zero or negative!")
-        self.__fuel_amount += fuel
-        if self.__fuel_amount > self.__fuel_capacity:
-            self.__fuel_amount = self.__fuel_capacity
+    def test_no_make_raises_ex(self):
+        with self.assertRaises(Exception) as ex:
+            self.lambo.make = ""
 
-    def drive(self, distance):
-        needed = (distance / 100) * self.__fuel_consumption
+        self.assertEqual("Make cannot be null or empty!", str(ex.exception))
 
-        if needed > self.__fuel_amount:
-            raise Exception("You don't have enough fuel to drive!")
+    def test_no_model_raises_ex(self):
+        with self.assertRaises(Exception) as ex:
+            self.lambo.model = ""
 
-        self.__fuel_amount -= needed
+        self.assertEqual("Model cannot be null or empty!", str(ex.exception))
 
-car = Car("a", "b", 1, 4)
-car.make = ""
-print(car)
+    def test_fuel_consumption_cannot_be_negative_or_zero(self):
+        with self.assertRaises(Exception) as ex:
+            self.lambo.fuel_consumption = -1
+
+        self.assertEqual("Fuel consumption cannot be zero or negative!",
+                         str(ex.exception))
+
+        with self.assertRaises(Exception) as ex:
+            self.lambo.fuel_consumption = 0
+
+        self.assertEqual("Fuel consumption cannot be zero or negative!",
+                         str(ex.exception))
+
+    def test_fuel_capacity_cannot_be_negative_or_zero(self):
+        with self.assertRaises(Exception) as ex:
+            self.lambo.fuel_capacity = -1
+
+        self.assertEqual("Fuel capacity cannot be zero or negative!",
+                         str(ex.exception))
+
+        with self.assertRaises(Exception) as ex:
+            self.lambo.fuel_capacity = 0
+
+        self.assertEqual("Fuel capacity cannot be zero or negative!",
+                         str(ex.exception))
+
+        def test_fuel_capacity_cannot_be_negative_or_zero(self):
+            with self.assertRaises(Exception) as ex:
+                self.lambo.fuel_capacity = -1
+
+            self.assertEqual("Fuel capacity cannot be zero or negative!",
+                             str(ex.exception))
+
+            with self.assertRaises(Exception) as ex:
+                self.lambo.fuel_capacity = 0
+
+            self.assertEqual("Fuel capacity cannot be zero or negative!",
+                             str(ex.exception))
+
+    def test_fuel_amount_cannot_be_negative(self):
+        with self.assertRaises(Exception) as ex:
+            self.lambo.fuel_amount = -1
+
+        self.assertEqual("Fuel amount cannot be negative!",
+                         str(ex.exception))
+
+    def test_cannot_refuel_with_zero_raises_exception(self):
+        with self.assertRaises(Exception) as ex:
+            self.lambo.refuel(0)
+
+        self.assertEqual("Fuel amount cannot be zero or negative!",
+                         str(ex.exception))
+
+    def test_refuel_not_full_tank(self):
+        self.lambo.refuel(10)
+        self.assertEqual(10, self.lambo.fuel_amount)
+
+    def test_refuel_with_more_than_capacity_expect_fill_to_capacity(self):
+        self.lambo.refuel(100)
+        self.assertEqual(80, self.lambo.fuel_amount)
+
+    def test_drive_if_not_enough_fuel_raises_exception(self):
+        self.lambo.fuel_amount = 20
+
+        with self.assertRaises(Exception) as ex:
+            self.lambo.drive(1000)
+
+        self.assertEqual("You don't have enough fuel to drive!",
+                         str(ex.exception))
+
+    def test_drive_with_enough_fuel_expect_fuel_amount_decrease(self):
+        self.lambo.fuel_amount = 80
+
+        self.lambo.drive(100)
+
+        self.assertEqual(70, self.lambo.fuel_amount)
+
+
+
+
+
+if __name__ == "__main__":
+    unittest.main()
